4e3ddc98cc37a15f79b18ebb74854caef23bce17
Vikas Bhavsar <vikasbhvsr@gmail.com>
2018-12-11 14:24:07 -0800

    favicon change

--- app.js
+++ app.js
@@ -13,12 +13,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 //Look at public folder
